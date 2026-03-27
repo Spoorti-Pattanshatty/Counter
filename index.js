76b03aa3057c9551df7c9a60b8180cf090029d4a
Spoorti P <spoortippattanshatty@gmail.com>
2026-03-27 11:27:41 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ let count = 0;
 
 function increase(){
     count++;
-    document.getElementById("ctr_data").innerHTML=count;
+    document.getElementById("count").innerHTML=count;
 }
 
 function decrease(){
    
     if(count>=1){
         count--;
-        document.getElementById("ctr_data").innerHTML=count;
+        document.getElementById("count").innerHTML=count;
     }
 }
 
+function reset() {
+    count = 0;
+    document.getElementById("count").innerText = count;
+}
 
 
